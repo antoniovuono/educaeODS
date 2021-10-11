8067a32dd75d9ab5183883550ac63511244c59ee
Antonio Sampaio De Vuono <antoniosvuono@icloud.com>
2021-10-10 23:29:06 -0300

    first

---
+++ src/pages/screens/Home/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import * as Styled from './styles';
+
+const Home: React.FC = () => {
+  return (
+    <Styled.Container>
+      <Styled.Title>Olá mundo!</Styled.Title>
+    </Styled.Container>
+  );
+};
+
+export default Home;
